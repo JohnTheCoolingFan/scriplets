b624bfff214a8a4abd0f7535c139234b741a832f
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-03-10 00:09:05 +0300

    Unified DataValue

--- src/data_value.rs
+++ src/data_value.rs
@@ -1,55 +1,45 @@
 //! Enums for representing data stored in data storages. Takes inspiration from mlua's Value.
 
+use bevy::utils::FloatOrd;
 use mlua::prelude::*;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use thiserror::Error;
+use std::collections::BTreeMap;
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 #[serde(untagged)]
 pub enum DataValue {
     Nil,
     Boolean(bool),
-    Integer(LuaInteger),
-    Number(LuaNumber),
+    Integer(i64),
+    #[serde(with = "floatord")]
+    Number(FloatOrd),
     String(String),
     Sequence(Vec<DataValue>),
-    Table(HashMap<DataValueHashEq, DataValue>),
+    Table(BTreeMap<DataValue, DataValue>),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Hash, PartialEq, Eq)]
-#[serde(untagged)]
-pub enum DataValueHashEq {
-    Nil,
-    Boolean(bool),
-    Integer(LuaInteger),
-    String(String),
-    Sequence(Vec<DataValueHashEq>),
+mod floatord {
+    use bevy::utils::FloatOrd;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    pub fn serialize<S>(val: &FloatOrd, ser: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        val.0.serialize(ser)
+    }
+
+    pub fn deserialize<'de, D>(deser: D) -> Result<FloatOrd, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Ok(FloatOrd(f32::deserialize(deser)?))
+    }
 }
 
 impl Default for DataValue {
     fn default() -> Self {
         Self::Nil
-    }
-}
-
-impl Default for DataValueHashEq {
-    fn default() -> Self {
-        Self::Nil
-    }
-}
-
-impl From<DataValueHashEq> for DataValue {
-    fn from(data: DataValueHashEq) -> Self {
-        match data {
-            DataValueHashEq::Nil => Self::Nil,
-            DataValueHashEq::Boolean(b) => Self::Boolean(b),
-            DataValueHashEq::Integer(i) => Self::Integer(i),
-            DataValueHashEq::String(s) => Self::String(s),
-            DataValueHashEq::Sequence(sq) => {
-                Self::Sequence(sq.into_iter().map(Into::into).collect())
-            }
-        }
     }
 }
 
@@ -60,7 +50,7 @@ impl<'lua> FromLua<'lua> for DataValue {
             LuaValue::Nil => Ok(Self::Nil),
             LuaValue::Boolean(b) => Ok(Self::Boolean(b)),
             LuaValue::Integer(i) => Ok(Self::Integer(i)),
-            LuaValue::Number(n) => Ok(Self::Number(n)),
+            LuaValue::Number(n) => Ok(Self::Number(FloatOrd(n as f32))),
             LuaValue::String(s) => Ok(Self::String(s.to_str()?.into())),
             LuaValue::Table(t) => {
                 if let Ok(seq) = t
@@ -72,7 +62,7 @@ impl<'lua> FromLua<'lua> for DataValue {
                 } else {
                     Ok(Self::Table(
                         t.pairs()
-                            .collect::<LuaResult<HashMap<DataValueHashEq, DataValue>>>()?,
+                            .collect::<LuaResult<BTreeMap<DataValue, DataValue>>>()?,
                     ))
                 }
             }
@@ -91,52 +81,10 @@ impl<'lua> ToLua<'lua> for DataValue {
             Self::Nil => Ok(LuaValue::Nil),
             Self::Boolean(b) => Ok(LuaValue::Boolean(b)),
             Self::Integer(i) => Ok(LuaValue::Integer(i)),
-            Self::Number(n) => Ok(LuaValue::Number(n)),
+            Self::Number(n) => Ok(LuaValue::Number(n.0.into())),
             Self::String(s) => s.to_lua(lua),
             Self::Sequence(seq) => seq.to_lua(lua),
             Self::Table(t) => t.to_lua(lua),
         }
     }
-}
-
-impl TryFrom<DataValue> for DataValueHashEq {
-    type Error = DataValueConversionError;
-
-    fn try_from(value: DataValue) -> Result<Self, Self::Error> {
-        match value {
-            DataValue::Nil => Ok(Self::Nil),
-            DataValue::Boolean(b) => Ok(Self::Boolean(b)),
-            DataValue::Integer(i) => Ok(Self::Integer(i)),
-            DataValue::Number(n) => Err(Self::Error::Number(n)),
-            DataValue::String(s) => Ok(Self::String(s)),
-            DataValue::Sequence(sq) => Ok(Self::Sequence(
-                sq.into_iter()
-                    .map(TryInto::try_into)
-                    .collect::<Result<Vec<Self>, Self::Error>>()?,
-            )),
-            DataValue::Table(t) => Err(Self::Error::Table(t)),
-        }
-    }
-}
-
-impl<'lua> FromLua<'lua> for DataValueHashEq {
-    fn from_lua(lua_value: LuaValue<'lua>, lua: &'lua Lua) -> LuaResult<Self> {
-        DataValue::from_lua(lua_value, lua)?
-            .try_into()
-            .map_err(LuaError::external)
-    }
-}
-
-impl<'lua> ToLua<'lua> for DataValueHashEq {
-    fn to_lua(self, lua: &'lua Lua) -> LuaResult<LuaValue<'lua>> {
-        DataValue::from(self).to_lua(lua)
-    }
-}
-
-#[derive(Debug, Clone, Error)]
-pub enum DataValueConversionError {
-    #[error("DataValueHashEq can't contain f64")]
-    Number(LuaNumber),
-    #[error("DataValueHashEq can't contain HashMap")]
-    Table(HashMap<DataValueHashEq, DataValue>),
 }
